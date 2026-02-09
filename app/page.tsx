7e5fda741c01ce221b20d234115c1312ca6a8d79
Rai <nurairstalk@proton.me>
2026-02-09 18:51:45 +0700

    remove padding on livetracker component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 space-y-12 pb-20 pt-8">
         {/* Market List */}
         <section className="space-y-6">
-          <div className="max-w-full my-5 mx-auto px-4">
+          <div className="max-w-full my-5 mx-auto md:px-0">
             <LivePriceTicker />
           </div>
           <div className="flex items-center gap-3 mb-6">
